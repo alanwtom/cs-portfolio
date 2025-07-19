98ffe6754e7e208a62bd90559dfa2031742449e7
alantomw <alantomw@users.noreply.github.com>
2025-07-18 23:27:22 -0400

    added embed

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,28 @@ import "./globals.css";
 export const metadata: Metadata = {
   title: "Alan Tom",
   description: "my portfolio",
+  openGraph: {
+    title: "jobmaxxing",
+    description: "Alan Tom's Portfolio",
+    url: "https://alantom.dev",
+    siteName: "Alan Tom",
+    images: [
+      {
+        url: "/images/cockatiel.webp",
+        width: 1200,
+        height: 630,
+        alt: "Alan Tom's Portfolio",
+      },
+    ],
+    locale: "en_US",
+    type: "website",
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "jobmaxxing",
+    description: "Alan Tom's Portfolio",
+    images: ["/images/cockatiel.webp"],
+  },
 };
 
 export default function RootLayout({
